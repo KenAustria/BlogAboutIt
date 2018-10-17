d9d7013682c71e69ee5028c7a7a63a4899c915cb
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-10-17 15:00:41 -0700

    Redux setup

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import PostsIndex from './containers/PostsIndex';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
- <BrowserRouter>
-  <div>
-   <Route path="/" component={PostsIndex} />
-  </div>
- </BrowserRouter>,
+ <Provider store={store}>
+  <BrowserRouter>
+   <div>
+    <Route path="/" component={PostsIndex} />
+   </div>
+  </BrowserRouter>
+ </Provider>,
  document.getElementById('root')
 );
 

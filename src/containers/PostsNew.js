c5bdd1b65a6bb66943c157a3d6dfcc6e64625014
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-10-26 20:33:17 -0700

    Removed Link tag wrapping Submit Button

--- src/containers/PostsNew.js
+++ src/containers/PostsNew.js
@@ -1,113 +1,91 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import { connect } from "react-redux";
 import { createPost } from "../actions/index";
-import { bindActionCreators } from "redux";
 
 class PostsNew extends Component {
-	renderField(field) {
-		const { meta: {touched, error}} = field;
-		const className = `form_group_field ${touched && error ? 'invalid_feedback' : ''}`;
+  renderField(field) {
+    const {meta: { touched, error }} = field;
+    const className = `form_group_field ${touched && error ? "invalid_feedback" : ""}`;
 
-		return (
-			<div className={className}>
-				<div className="new_labels">
-					<label>{field.label}</label>
-				</div>
-				<input
-					type="text"
-					className="form_input"
-					{...field.input}
-				/>
-				<div className="text-help">
-					{touched ? error : ''}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className={className}>
+        <div className="new_labels">
+          <label>{field.label}</label>
+        </div>
+        <input type="text" className="form_input" {...field.input} />
+        <div className="text-help">{touched ? error : ""}</div>
+      </div>
+    );
+  }
 
-	renderFieldArea(field) {
-		const { meta: {touched, error}} = field;
-		const className = `form_group_field ${touched && error ? 'invalid_feedback' : ''}`;
+  renderFieldArea(field) {
+    const {meta: { touched, error }} = field;
+    const className = `form_group_field ${touched && error ? "invalid_feedback" : ""}`;
 
-		return (
-			<div className={className}>
-				<div className="new_labels">
-					<label>{field.label}</label>
-				</div>
-				<textarea
-					type="text"
-					className="form_input"
-					{...field.input}
-				/>
-				<div className="text-help">
-					{touched ? error : ''}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className={className}>
+        <div className="new_labels">
+          <label>{field.label}</label>
+        </div>
+        <textarea type="text" className="form_input" {...field.input} />
+        <div className="text-help">{touched ? error : ""}</div>
+      </div>
+    );
+  }
 
-	onSubmit(values) {
+  onSubmit(values) {
 		this.props.createPost(values, () => {
-			this.props.history.push('/');
-		});
-	}
+      this.props.history.push("/");
+    });
+  }
 
-	render() {
-		const { handleSubmit } = this.props;
+  render() {
+    const { handleSubmit } = this.props;
 
-		return (
-			<div className="new_group">
-				<div className="new_title">Create New</div>
-					<div className="form_group">
-						<form onSubmit={handleSubmit(this.onSubmit.bind(this))} >
-							<Field label="Title" name="title" component={this.renderField} />
-							<Field label="Categories" name="categories" component={this.renderField} />
-							<Field label="Content" name="content" component={this.renderFieldArea} />
-						</form>
+    return (
+      <div className="new_group">
+        <div className="new_title">Create New</div>
+        <div className="form_group">
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <Field label="Title" name="title" component={this.renderField} />
+            <Field label="Categories" name="categories" component={this.renderField} />
+            <Field label="Content" name="content" component={this.renderFieldArea} />
 						<div className="new_buttons">
 							<Link to="/" className="remove_link">
-								<Button variant="outlined" color="secondary">Cancel</Button>
-							</Link>
+              	<Button variant="outlined" color="secondary">Cancel</Button>
+            	</Link>
 							<div className="divider"></div>
-							<Link to="/" className="remove_link">
-								<Button variant="outlined" color="primary" type="submit">
-									Submit
-								</Button>
-							</Link>
-							
+            	<Button variant="outlined" color="primary" type="submit">Submit</Button>
 						</div>
-					</div>
-			</div>
-		);
-	}
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 function validate(values) {
-	const errors = {};
+  const errors = {};
 
-	if (!values.title) {
-		errors.title = 'Enter a title';
-	}
-	if (!values.categories) {
-		errors.categories = 'Enter a category';
-	}
-	if (!values.content) {
-		errors.content = 'Enter some content';
-	}
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a category";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
 
-	return errors;
-}
-
-function mapDispatchToProps(dispatch) {
-	return bindActionCreators({ createPost }, dispatch);
+  return errors;
 }
 
 export default reduxForm({
-	validate,
-	form: 'PostsNewForm'
+  validate,
+  form: "PostsNewForm"
 })(
-	connect(null, mapDispatchToProps)(PostsNew)
+  connect(null, { createPost })(PostsNew)
 );
